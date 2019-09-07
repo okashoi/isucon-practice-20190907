d41259d4c0a4b943e8a57d4ef27008dbb4b62e97
Okada Shohei <okashoicircus0@gmail.com>
2019-09-07 16:08:21 +0900

    デプロイスクリプトを作成

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+git pull
+cd /home/isucon/torb/webapp/go && make
+
+sudo systemctl stop torb.go
+sudo systemctl stop h2o
+sudo systemctl stop mariadb
+
+# ログ消す
+sudo rm -f /var/log/mariadb/mariadb.log
+sudo rm -f /var/log/mariadb/slow.log
+sudo rm -f /var/log/h2o/access.log
+sudo rm -f /var/log/h2o/error.log
+
+sudo systemctl daemon-reload
+
+sudo systemctl start mariadb
+sudo systemctl start h2o
+sudo systemctl start torb.go
